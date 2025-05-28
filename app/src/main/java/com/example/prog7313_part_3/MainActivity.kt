72f732ec1ee3b24e31072068e49bd48d3d1fa52f
soyama <Soyamapango15@gmail.com>
2025-05-28 16:51:29 +0200

    Monthly saving goal

--- app/src/main/java/com/example/prog7313_part_3/MainActivity.kt
+++ app/src/main/java/com/example/prog7313_part_3/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+   /*
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -17,4 +19,5 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+*/
 }
