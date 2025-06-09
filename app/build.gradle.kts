0887f99b4234aacc112e32096003462e680cd175
soyama <Soyamapango15@gmail.com>
2025-06-09 17:46:40 +0200

    Graph

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,9 +44,12 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
 
+
     implementation("androidx.room:room-runtime:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
